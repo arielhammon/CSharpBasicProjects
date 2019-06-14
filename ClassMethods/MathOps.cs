9ef8a74d0ca36b4e16619d5bdbfc99f042f1f753
Ariel Hammon <arielhammon@gmail.com>
2019-06-13 22:29:49 -0700

    add C# console app demonstrating basic class properties, math operations, and overflow checking
    This C# console app demonstrates basic class properties, math operations, and overflow checking.

---
+++ ClassMethods/MathOps.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethods
+{
+    public static class MathOps
+    {
+        static public int TwoToPowerOf(int n)
+        //returns 2^n
+        {
+            int nn = 1;
+            for (int i = 1; i < n; i++)
+            {
+                checked { nn *= 2; }
+            }
+            return nn;
+        }
+        static public int Factorial(int n)
+        //returns n! = n(n-1)(n-2)*...*3*2*1
+        {
+            int nn = 1;
+            for (int i = 1; i < n + 1; i++)
+            {
+                checked { nn *= i; }
+            }
+            return nn;
+        }
+
+        static public int RaiseToPowerOfSelf(int n)
+        //returns n^n
+        {
+            int nn = 1;
+            for (int i = 0; i < n; i++)
+            {
+                checked { nn *= n; }
+            }
+            return nn;
+        }
+    }
+}
---
+++ ClassMethods/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            string play = "y";
+            do
+            {
+                try
+                {
+                    Console.Write("Let's do some math. Enter a whole number. I recommend keeping it small:  ");
+                    n = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("2^{0} (exponentiation) is: {1}.", n, MathOps.TwoToPowerOf(n));
+                    Console.WriteLine("{0}! (factorial) is: {1}.", n, MathOps.Factorial(n));
+                    Console.WriteLine("{0}^{0} (exponentiation) is: {1}.", n, MathOps.RaiseToPowerOfSelf(n));
+                    Console.WriteLine("See, these mathematical operations grow quickly!");
+                    Console.Write("Wanna go again? y/n:  ");
+                    play = Console.ReadLine();
+                    Console.WriteLine();
+                }
+                    
+                catch (OverflowException ex)
+                {
+                    Console.WriteLine("Sorry, the number was too big to complete the calculation!");
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Sorry, the input was not a whole number.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            } while (play != "n");
+        }
+    }
+}
