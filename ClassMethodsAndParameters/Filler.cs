c494be5ed9775e0c701ff5f3c9ad492644fd195e
Ariel Hammon <arielhammon@gmail.com>
2019-06-17 21:42:24 -0700

    add short C# demo of class methods and named or unnamed parameters
    A short C# demo of class methods and named or unnamed parameters.

---
+++ ClassMethodsAndParameters/Filler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMethodsAndParameters
+{
+    class Filler
+    {
+        public List<int> MList { get; set; }
+        public Filler()
+        {
+            MList = new List<int>();
+        }
+
+        public void FillMultiples(int firstNum, int howMany)
+        {
+            int n = firstNum;
+            for (int i = 0; i < howMany; i++)
+            {
+                MList.Add(n);
+                Console.Write(n + " ");
+                n += firstNum;
+            }
+            Console.WriteLine();
+            Console.WriteLine("We have added {0} multiples of {1} to the list.", howMany, firstNum);
+        }
+    }
+}
---
+++ ClassMethodsAndParameters/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClassMethodsAndParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Filler MyFiller = new Filler();
+            
+            //here we call a method by passing literals
+            MyFiller.FillMultiples(5, 20);
+
+            //now here we call the same method by passing variables using named parameters
+            int m = 7;
+            int n = 6;
+            MyFiller.FillMultiples(firstNum: m, howMany: n);
+
+            Console.ReadLine();
+        }
+    }
+}
