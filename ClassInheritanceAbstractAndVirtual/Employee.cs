81f5e73dc208c0027249b564fc512cea704083a8
Ariel Hammon <arielhammon@gmail.com>
2019-06-20 10:18:47 -0700

    add C# demo of abstract classes and abstract/virtual methods
    This is a basic C# demo of abstract classes and abstract/virtual methods

---
+++ ClassInheritanceAbstractAndVirtual/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassInheritanceAbstractAndVirtual
+{
+    public class Employee : Person
+    {
+        public int ID { get; set; }
+        public string Certs { get; set; }
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + base.GetFullName());
+        }
+
+        public override string GetFullName()
+        {
+            //adds certificaitons after the full name
+            return base.GetFullName() + (string.IsNullOrEmpty(Certs) ? "" : ", " + Certs);
+        }
+    }
+}
