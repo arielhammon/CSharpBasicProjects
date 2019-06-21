80d842e8966c3f648734c32221f4f1e07711fbfe
Ariel Hammon <arielhammon@gmail.com>
2019-06-20 18:38:35 -0700

    add C# demo which implements generic types with overloaded comparison and casting operators
    This C# demo implements generic types with overloaded comparison and casting operators

---
+++ ClassInheritanceGenericTypes/Employee.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassInheritanceAbstractAndVirtual
+{
+    public class Employee<T> : Person, IQuittable where T : IConvertible
+    {
+
+        public List<T> things { get; set; }
+
+        public Employee()
+        {
+            things = new List<T>();
+        }
+        public int ID { get; set; }
+        public string Certs { get; set; }
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + base.GetFullName());
+        }
+
+        public override string GetFullName()
+        {
+            //adds certificaitons after the full name
+            return base.GetFullName() + (string.IsNullOrEmpty(Certs) ? "" : ", " + Certs);
+        }
+
+        public void Quit()
+        {
+            string message;
+            message = "Employee ID: " + ID + "\n";
+            message += "Name: " + GetFullName() + "\n";
+            message += "Quit our grand establishment on " + DateTime.Now.ToString() + ".";
+            Console.WriteLine(message);
+        }
+
+        public static bool operator ==(Employee<T> emp1, Employee<T> emp2)
+        {
+            if (emp1 is null && emp2 is null) return true;
+            if (emp1 is null && !(emp2 is null) ||
+                !(emp1 is null) && emp2 is null) return false;
+            if (!emp1.GetType().Equals(emp2.GetType())) //in case a class is derived from this class
+            {
+                return false;
+            }
+            else
+            {
+                return emp1.ID == emp2.ID;
+            }
+        }
+
+        public static bool operator ==(Employee<string> emp1, Employee<T> emp2)
+        {
+            return (Employee<T>)emp1 == emp2;
+        }
+
+        public static bool operator ==(Employee<int> emp1, Employee<T> emp2)
+        {
+            return (Employee<T>)emp1 == emp2;
+        }
+
+        public static bool operator!= (Employee<T> emp1, Employee<T> emp2)
+        {
+            return !(emp1 == emp2);
+        }
+
+        public static bool operator !=(Employee<string> emp1, Employee<T> emp2)
+        {
+            return !(emp1 == emp2);
+        }
+        public static bool operator !=(Employee<int> emp1, Employee<T> emp2)
+        {
+            return !(emp1 == emp2);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if ((obj is null) || !this.GetType().Equals(obj.GetType())) //in case a class is derived from this class
+            {
+                return false;
+            }
+            else
+            {
+                Employee<T> emp = (Employee<T>)obj; //again in case a class is derived from this class
+                return ID == emp.ID;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return ID;
+        }
+
+        public static Employee<T> operator+ (Employee<T> emp1, Employee<T> emp2)
+        //returns a new employee with an ID one greater than that of the argument with the largest ID
+        //the new employee has the combined certifications of the arguments
+        //I know this is kind of lame, especially since it doesn't guarantee uniqueness of ID, but it's just for demo purposes :)
+        {
+            int id = Math.Max(emp1.ID, emp2.ID) + 1;
+            string certs = emp1.Certs + ", " + emp2.Certs;
+            Employee<T> emp = new Employee<T>() { ID = id, Certs = certs };
+            return emp;
+        }
+
+        private static T MyChangeType(object fromThing, Type toType)
+        {
+            if (fromThing.GetType() == toType) return (T)fromThing;
+            try
+            {
+                return (T)Convert.ChangeType(fromThing, toType);
+            }
+            catch (InvalidCastException)
+            {
+                return default;
+            }
+            catch (FormatException)
+            {
+                return default;
+            }
+        }
+
+        public static implicit operator Employee<T>(Employee<int> emp1)
+        {
+            Employee<T> temp = new Employee<T>
+            {
+                ID = emp1.ID,
+                FirstName = emp1.FirstName,
+                LastName = emp1.LastName,
+                Certs = emp1.Certs
+            };
+            foreach (int thing in emp1.things) temp.things.Add(MyChangeType(thing, typeof(T)));
+            return temp;
+        }
+
+        public static implicit operator Employee<T>(Employee<string> emp1)
+        {
+            Employee<T> temp = new Employee<T>
+            {
+                ID = emp1.ID,
+                FirstName = emp1.FirstName,
+                LastName = emp1.LastName,
+                Certs = emp1.Certs
+            };
+            foreach (string thing in emp1.things) temp.things.Add(MyChangeType(thing, typeof(T)));
+            return temp;
+        }
+    }
+}
---
+++ ClassInheritanceGenericTypes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ClassInheritanceAbstractAndVirtual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> emp1 = new Employee<string>() { FirstName = "Sample", LastName = "Student", ID = 1234, Certs = "MCSD" };
+            emp1.things.AddRange(new string[] { "stapler", "printer", "marker board", "1" });
+            emp1.SayName();
+            Console.WriteLine("Professionally known as: " + emp1.GetFullName());
+            Console.WriteLine("Here is a list of this employee's things:");
+            foreach (string thing in emp1.things) Console.WriteLine(thing);
+
+            Employee<int> emp2 = new Employee<int>() { FirstName = "Sample", LastName = "Professor", ID = 1235, Certs = "PhD" };
+            emp2.things.AddRange(new int[] { 2, 3, 5, 7 });
+            emp2.SayName();
+            Console.WriteLine("Professionally known as: " + emp2.GetFullName());
+            Console.WriteLine("Here is a list of this employee's things:");
+            foreach (int thing in emp2.things) Console.WriteLine(thing);
+            Console.WriteLine("These two employees are the same: {0}.", emp1 == emp2);
+
+            Employee<string> emp3 = emp1 + (Employee<string>)emp2;
+            emp3.FirstName = "Buzz";
+            emp3.LastName = "Lightyear";
+            emp3.SayName();
+            Console.WriteLine("This employee has certifications: {0}.", emp3.Certs);
+
+            Console.WriteLine();
+            Console.WriteLine("Update:");
+            emp1.Quit();
+            Console.ReadLine();
+        }
+    }
+}
