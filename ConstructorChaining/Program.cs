1a38b8f1a05504eb357341d1dfc5854050d255fa
Ariel Hammon <arielhammon@gmail.com>
2019-07-03 15:29:09 -0700

    add C# constructor chaining demo with class that generates random numbers by type

---
+++ ConstructorChaining/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConstructorChaining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new RandomByType();
+            Console.WriteLine("Here's a random 8 bit integer: " + random.ByteNext);
+            Console.WriteLine("Here's a random 16 bit integer: " + random.ShortNext);
+            Console.WriteLine("Here's a random 16 bit unsigned integer: " + random.UShortNext);
+            Console.WriteLine("Here's a random 32 bit integer: " + random.IntNext);
+            Console.WriteLine("Here's a random 32 bit unsigned integer: " + random.UIntNext);
+            Console.WriteLine("Here's a random 64 bit integer: " + random.LongNext);
+            Console.WriteLine("Here's a random 64 bit integer: " + random.ULongNext);
+            Console.WriteLine("Here's a random boolean: " + random.BoolNext);
+            Console.WriteLine("Here's a random 64 bit float: " + random.DoubleNext);
+            Console.WriteLine("Here's a random 32 bit float: " + random.FloatNext);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConstructorChaining/RandomByType.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace ConstructorChaining
+{
+    class RandomByType
+    {
+        //These constants represent the number of bytes in x bits
+        const int bits8 = 1;
+        const int bits16 = 2;
+        const int bits32 = 4;
+        const int bits64 = 8;
+
+        private Random random;
+        public RandomByType(int seed)
+        {
+            random = new Random(seed);
+        }
+        public RandomByType() : this(DateTime.Now.GetHashCode()) { }
+
+        public int IntNext
+        {
+            get
+            {
+                var bytes = new byte[bits32];
+                random.NextBytes(bytes);
+                return BitConverter.ToInt32(bytes, startIndex: 0);
+            }
+        }
+        public int IntNextMinMax(int min, int max)
+        {
+            return random.Next(min, max);
+        }
+        public int IntNextMax(int max)
+        {
+            return random.Next(max);
+        }
+        public uint UIntNext
+        {
+            get
+            {
+                var bytes = new byte[bits32];
+                random.NextBytes(bytes);
+                return BitConverter.ToUInt32(bytes, startIndex: 0);
+            }
+        }
+        public long LongNext
+        {
+            get
+            {
+                var bytes = new byte[bits64];
+                random.NextBytes(bytes);
+                return BitConverter.ToInt64(bytes, startIndex: 0);
+            }
+        }
+        public ulong ULongNext
+        {
+            get
+            {
+                var bytes = new byte[bits64];
+                random.NextBytes(bytes);
+                return BitConverter.ToUInt64(bytes, startIndex: 0);
+            }
+        }
+        public short ShortNext
+        {
+            get
+            {
+                var bytes = new byte[bits16];
+                random.NextBytes(bytes);
+                return BitConverter.ToInt16(bytes, startIndex: 0);
+            }
+        }
+        public ushort UShortNext
+        {
+            get
+            {
+                var bytes = new byte[bits16];
+                random.NextBytes(bytes);
+                return BitConverter.ToUInt16(bytes, startIndex: 0);
+            }
+        }
+        public byte ByteNext
+        {
+            get
+            {
+                var bytes = new byte[bits8];
+                random.NextBytes(bytes);
+                return bytes[0];
+            }
+        }
+        public double DoubleNext
+        {
+            get
+            {
+                var bytes = new byte[bits64];
+                random.NextBytes(bytes);
+                return BitConverter.ToDouble(bytes, startIndex: 0);
+            }
+        }
+        public double DoubleNext0to1
+        {
+            get
+            {
+                return random.NextDouble();
+            }
+        }
+        public float FloatNext
+        {
+            get
+            {
+                var bytes = new byte[bits32];
+                random.NextBytes(bytes);
+                return BitConverter.ToSingle(bytes, startIndex: 0);
+            }
+        }
+        public bool BoolNext
+        {
+            get
+            {
+                var bytes = new byte[bits8];
+                random.NextBytes(bytes);
+                return BitConverter.ToBoolean(bytes, startIndex: 0);
+            }
+        }
+    }
+}
