b3f6a37556f7e36c8b297509d3c8421d503216f1
Ariel Hammon <arielhammon@gmail.com>
2019-07-03 11:49:29 -0700

    add C# demo of DateTime and System.Timers

---
+++ DateAndTime/LocalTimer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Timers;
+
+namespace DateAndTime
+{
+    class LocalTimer
+    {
+        public DateTime endTime { get; set; }
+        public Timer timer { get; set; }
+        private bool _active;
+        private int intervals; //counts how many intervals have elapsed
+        private int indicatorIntervals; //the number of intervals between indicator dots
+        public bool active
+        {
+            get
+            {
+                return _active;
+            }
+            set
+            {
+                _active = value;
+                if (value)
+                {
+                    timer.Elapsed += Timer_Elapsed;
+                }
+                else
+                {
+                    timer.Elapsed -= Timer_Elapsed;
+                }
+            }
+        }
+        public LocalTimer(DateTime setEndTime, int indicatorIntervalsDelay = 5)
+        {
+            endTime = setEndTime;
+            timer = new Timer
+            {
+                Interval = 1000 //the timer event will fire each second (1000ms)
+            };
+            intervals = 0;
+            indicatorIntervals = indicatorIntervalsDelay;
+            active = true;
+            timer.Start();
+        }
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            intervals++;
+            DateTime currentTime = DateTime.Now;
+            if (currentTime.Date == endTime.Date && currentTime.Hour == endTime.Hour && 
+                currentTime.Minute == endTime.Minute && currentTime.Second == endTime.Second)
+            {
+                timer.Stop();
+                active = false;
+                Console.WriteLine("Time's up!");
+                Console.WriteLine("It is now: " + endTime);
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.Beep(1000, 200);
+                }
+            }
+            else
+            {
+                if (intervals % indicatorIntervals == 0) Console.Write(".");
+            }
+        }
+    }
+}
---
+++ DateAndTime/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DateAndTime
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to our basic date and time demo.");
+            Console.WriteLine("It is currently: " + DateTime.Now + " local time.");
+            Console.WriteLine("Let's set a timer.");
+            Console.Write("How many hours from now?: ");
+            string temp = Console.ReadLine();
+            int hours = int.Parse(temp);
+            Console.Write("How many minutes from now?: ");
+            temp = Console.ReadLine();
+            int minutes = int.Parse(temp);
+            Console.Write("How many seconds from now?: ");
+            temp = Console.ReadLine();
+            int seconds = int.Parse(temp);
+
+            double milliSeconds = 1000*(hours * 3600 + minutes * 60 + seconds);
+            TimeSpan timeLength = TimeSpan.FromMilliseconds(milliSeconds);
+            DateTime startTime = DateTime.Now;
+            DateTime endTime = DateTime.Now.Add(timeLength);
+            Console.WriteLine("Timer started at: " + DateTime.Now + " local time.");
+            LocalTimer timer = new LocalTimer(endTime); //declares a new instance of our custom timer class
+
+            Console.WriteLine("Please wait to be notified or enter \"cancel\" to cancel the timer.");
+            temp = Console.ReadLine();
+            if (temp == "cancel" && timer.active)
+            {
+                timer.active = false;
+                Console.WriteLine("Bye!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
